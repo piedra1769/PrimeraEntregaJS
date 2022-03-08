ece7da858f509f88a2409b8c30cd1dc5fe834113
piedra1769 <sebastianpiedrahitao@gmail.com>
2022-03-07 23:56:12 -0500

    primera entrega js

---
+++ cursoJS.js
@@ -0,0 +1,28 @@
+
+const cotizarViaje=()=>{
+    let destino=document.querySelector('inputDestino').value;
+    let personas=document.querySelector('inputPersonas').value;
+}
+
+
+
+const planes = [
+    {id: 1, destino: 'Santa Marta', precio: 500000},
+    {id: 2, destino: 'Cartagena', precio: 600000},
+    {id: 3, destino: 'Eje Cafetero', precio: 650000},
+]
+
+//Captura del valor destino
+function capturaruno () {
+    let dest = document.getElementById('inputDestino').value;
+}
+
+function capturar() {
+    let pers = document.getElementById('inputPersonas').value;
+}
+
+function cotizacion (precio, personas) {
+    return (precio * personas)
+    alert (cotizacion);
+}
+
